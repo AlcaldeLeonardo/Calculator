1c663fecbf4b18779e73644747b01478b574bdb4
Leonardo Esteban Alcalde Gómez <108157788+AlcaldeLeonardo@users.noreply.github.com>
2023-08-27 06:08:03 -0300

    optimiced code

--- script/index.js
+++ script/index.js
@@ -1,18 +1,9 @@
 import { captureKeys } from "./module/captureKeys.js";
+import { setEvents } from "./module/setEvents.js";
 
 const keyNum = [];
 const keyOp = [];
-const $keyDel = document.getElementById("keyDEL")
-const $keyClear = document.getElementById("keyClear");
-const $keyEqual = document.getElementById("keyEqual")
 
-const $displayOperands = document.getElementById("displayOperands");
-const $displayResult = document.getElementById("displayResult")
-
-let result = 0;
-
-
-let displayNum = "";
 
 
 //capturando los numeros
@@ -21,62 +12,6 @@ captureKeys(keyNum, "keyNum", 11);
 //capturando a los operadores del DOM
 captureKeys(keyOp, "keyOp", 4);
 
+setEvents(keyNum, keyOp);
 
-for (let index = 0; index < 11; index++) {
-    //agregando eventos a los numeros
-    keyNum[index].addEventListener("click", () =>{
-        let num = keyNum[index].innerText;
 
-        displayNum += `${num}`;
-
-        $displayOperands.innerHTML = displayNum;
-        console.log(displayNum)
-    })
-}
-
-for (let index = 0; index < 4; index++) {
-    
-    //agregando eventos a los operadores
-    keyOp[index].addEventListener("click", () =>{
-        if(displayNum != ""){
-            let operand = keyOp[index].innerText;
-            let lastChar = `${displayNum.charAt(displayNum.length - 2)}`
-    
-            console.log(displayNum.charAt(0));
-    
-            if(operand != lastChar) {
-                if(result == 0) displayNum += ` ${operand} `;
-                else {
-                    displayNum = `${result} ${operand} `
-                    result = 0;
-                }
-            }
-    
-            $displayOperands.innerHTML = displayNum;
-            console.log(displayNum)
-        }
-    })
-}
-
-$keyEqual.addEventListener("click", () =>{
-
-    if(displayNum == "") result = 0;
-    else result = eval(displayNum);
-    $displayResult.innerHTML = result;
-})
-
-$keyDel.addEventListener("click", ()=>{
-    displayNum = displayNum.trimEnd();
-
-    displayNum = displayNum.slice(0,-1);
-
-    $displayOperands.innerHTML = displayNum;
-})
-
-$keyClear.addEventListener("click", ()=>{
-    displayNum = "";
-    result = 0;
-    
-    $displayOperands.innerHTML = displayNum;
-    $displayResult.innerHTML = result;
-})
---
+++ script/module/setEvents.js
@@ -0,0 +1,70 @@
+export function setEvents(keyNum, keyOp) {
+    const $keyDel = document.getElementById("keyDEL");
+    const $keyClear = document.getElementById("keyClear");
+    const $keyEqual = document.getElementById("keyEqual");
+    const $displayOperands = document.getElementById("displayOperands");
+    const $displayResult = document.getElementById("displayResult");
+    let result = 0;
+    let displayNum = "";
+
+    function updateDisplay() {
+        $displayOperands.innerHTML = displayNum;
+    }
+
+    function handleNumberClick(num) {
+        displayNum += num;
+        updateDisplay();
+    }
+
+    function handleOperatorClick(operand) {
+        if (displayNum !== "") {
+            const lastChar = displayNum.charAt(displayNum.length - 2);
+            
+            if (operand !== lastChar) {
+                if (result === 0) {
+                    displayNum += ` ${operand} `;
+                } else {
+                    displayNum = `${result} ${operand} `;
+                    result = 0;
+                }
+            }
+
+            updateDisplay();
+        }
+    }
+
+    function handleEqualClick() {
+        result = displayNum === "" ? 0 : eval(displayNum);
+        $displayResult.innerHTML = result;
+    }
+
+    function handleDeleteClick() {
+        displayNum = displayNum.trimEnd().slice(0, -1);
+        updateDisplay();
+    }
+
+    function handleClearClick() {
+        displayNum = "";
+        result = 0;
+        updateDisplay();
+        $displayResult.innerHTML = result;
+    }
+
+    for (let index = 0; index < 11; index++) {
+        keyNum[index].addEventListener("click", () => {
+            handleNumberClick(keyNum[index].innerText);
+        });
+    }
+
+    for (let index = 0; index < 4; index++) {
+        keyOp[index].addEventListener("click", () => {
+            handleOperatorClick(keyOp[index].innerText);
+        });
+    }
+
+    $keyEqual.addEventListener("click", handleEqualClick);
+
+    $keyDel.addEventListener("click", handleDeleteClick);
+
+    $keyClear.addEventListener("click", handleClearClick);
+}
